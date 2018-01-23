fa55c2264c785444588eed3a6436452aacb42827
laurencantlin <laurenrose5321@gmail.com>
2018-01-22 22:53:00 -0600

    adds module function pattern and functions to generate random words

--- javascript.js
+++ javascript.js
@@ -1,1 +1,50 @@
-console.log("hi!");
+// var wordList = ["red", "gold", "blue", "orange", "yellow", "black", "white", "grey", "violet", "indigo", "silver", "beige"];
+
+var $ = function (sel) {
+    var elem = document.querySelectorAll(sel);
+    console.log(sel);
+    console.log(elem);
+
+    var text = function (textString) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].innerText = textString;
+        }
+    }
+
+    // GENERATE RANDOM WORD
+    var wordList = ["red", "gold", "blue", "orange", "yellow", "black", "white", "grey", "violet", "indigo", "silver", "beige"];
+
+    var generateWord = function () {
+        var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+        console.log(randomWord);
+        var letterArray = randomWord.split("");
+        console.log(letterArray);
+
+        for (var i = 0; i < elem.length; i++) {
+            // text = elem[i]
+            // elem[i].innerText = randomWord;
+            elem[i].innerHTML = letterArray;
+        }
+    }
+
+    var LetterArray = function (){
+
+    }
+
+    var on = function (action, cb) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(action, cb);
+        }
+    }
+
+        // BREAK WORD UP INTO ARRAY OF ITS COMPRISING LETTERS
+        var publicAPI = {
+            elem: elem,
+            wordList: wordList,
+            generateWord: generateWord,
+            text: text,
+            on: on
+        }
+
+        return publicAPI;
+    }
